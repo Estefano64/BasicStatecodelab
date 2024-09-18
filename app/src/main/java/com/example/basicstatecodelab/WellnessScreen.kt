cba0c85ef79f811bc34013eac8bcc77b772fbf23
Estefano64 <ulises.soto@tecsup.edu.pe>
2024-09-17 23:38:52 -0500

    codelab

---
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,8 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.basicstatecodelab.StatefulCounter
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    StatefulCounter(modifier)
+}
